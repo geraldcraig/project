5175d17bb9e6d0b098a45657b82e19f8a31d74bd
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-12-09 17:24:48 +0000

    function added

--- audio/main.py
+++ audio/main.py
@@ -1,7 +1,13 @@
 import os
 
-stem = "spleeter separate -p spleeter:2stems "
-folder = "-o output2 "
-audio_file = "audio_example.mp3"
 
-os.system(stem + folder + audio_file)
+# stem = "spleeter separate -p spleeter:2stems "
+# folder = "-o output2 "
+# audio_file = "audio_example.mp3"
+
+
+def process_audio(command):
+    return os.system(command)
+
+
+process_audio("spleeter separate -p spleeter:2stems -o output2 audio_example.mp3")
