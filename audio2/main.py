32b708f111fe03e261fa12edd3b23904af24e732
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-12-17 23:59:48 +0000

    updated

--- audio2/main.py
+++
@@ -1,9 +0,0 @@
-from spleeter.separator import Separator
-
-# Using embedded configuration.
-separator = Separator('spleeter:2stems')
-
-
-audio_file = "audio_example.mp3"
-folder = "output"
-separator.separate_to_file(audio_file, folder)
