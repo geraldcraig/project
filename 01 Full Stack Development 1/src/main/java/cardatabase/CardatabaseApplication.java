6f49606d2269c42d0d0f627d378317b08053eefa
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-01-01 12:46:00 +0000

    updated

--- 01 Full Stack Development 1/src/main/java/cardatabase/CardatabaseApplication.java
+++ 01 Full Stack Development 1/src/main/java/cardatabase/CardatabaseApplication.java
@@ -47,7 +47,7 @@ public class CardatabaseApplication {
 		
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));	
-			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner4));
 		};
 	}
 }
